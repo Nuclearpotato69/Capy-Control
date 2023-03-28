005cdd226d33ba751e4748238e934f2ce94da602
punchy <104541798+Nuclearpotato69@users.noreply.github.com>
2023-03-27 19:33:57 -0600

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,24 @@
+repeat
+	task.wait()
+until game:IsLoaded()
+_G.controller = "Main acc here"
+_G.rendering = false --false for no rendering (saves gpu)
+_G.fps = 30 --the fps the alts will run at 30 is recomended (helps with not killing cpu)
+
+alts = {
+    alt1 = "alt",--if you dont want all 10 to be filled put none there
+    alt2 = "none", 
+    alt3 = "none",
+    alt4 = "none",
+    alt5 = "none",
+    alt6 = "none",
+    alt7 = "none",
+    alt8 = "none",
+    alt9 = "none",
+    alt10 = "none"
+}
+if game:GetService("Players").LocalPlayer.Name == _G.controller then
+    print("Main acc detected")
+else
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Nuclearpotato69/Capy-Control/main/main.lua", true))()
+end
