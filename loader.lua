9a7741d77bfa8c717cd3368fb2bd29dd5b986cb3
punchy <104541798+Nuclearpotato69@users.noreply.github.com>
2025-01-23 21:39:24 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,9 @@
 -- Alt Control Script
-local prefix = "/" -- Command prefix
-local mainAccount = "MAIN ACCOUNT HERE" -- Main account to control alt accounts
-local fpslimit = 30 -- Default FPS limit for alt accounts
+_G.prefix = "/" -- Command prefix
+_G.mainAccount = "MAIN ACCOUNT HERE" -- Main account to control alt accounts
+_G.fpslimit = 30 -- Default FPS limit for alt accounts
 -- list of alt accounts to control
-local altAccounts = {
+_G.altAccounts = {
     ["ACCOUNT 1"] = true,
     ["ACCOUNT 2"] = true,
     ["ACCOUNT 3"] = true --copy this line and past it below to add more accounts (make sure after each line theres a comma (,))
